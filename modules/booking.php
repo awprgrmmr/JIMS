ace22d13bd0345d935599ea138cfecc87ac07fef
Aaron Walker <aw@prgrmmr.me>
2016-09-22 14:29:12 -0700

    Update booking module

--- modules/booking.php
+++ modules/booking.php
@@ -1,1 +1,46 @@
-This is the body of booking.php.
+<div id="booking">
+	<table>
+		<tr>
+			<th>#</th>
+			<th>Last, First</th>
+			<th>Date Booked</th>
+			<th>Offense</th>
+		</tr>
+		<tr>
+			<td>95092800</td>
+			<td>Urbanski, Aaron</td>
+			<td>09/28/1995</td>
+			<td>Forgery of a vehicle identification number</td>
+		</tr>
+		<tr>
+			<td>05011000</td>
+			<td>Mcintyre, Briana</td>
+			<td>01/10/2005</td>
+			<td>Inciting to riot</td>
+		</tr>
+		<tr>
+			<td>15122500</td>
+			<td>Hervey, James</td>
+			<td>12/25/2015</td>
+			<td>Criminal possession of an anti-security item</td>
+		</tr>
+		<tr>
+			<td>15122501</td>
+			<td>Khalil, Kyle</td>
+			<td>12/25/2015</td>
+			<td>Criminal sale of a controlled substance in or near school grounds</td>
+		</tr>
+		<tr>
+			<td>99032100</td>
+			<td>Hollister, Derek</td>
+			<td>03/21/1999</td>
+			<td>Endangering the welfare of a child</td>
+		</tr>
+		<tr>
+			<td>00113000</td>
+			<td>Schneps, Kevin</td>
+			<td>11/30/2000</td>
+			<td>Interference harassment or intimidation of a service animal</td>
+		</tr>
+	</table>
+</div>
