7abde3fcace06bdc04b443fd4fb9edd0e836e632
Aaron Walker <aw@prgrmmr.me>
2016-10-13 10:25:10 -0700

    Clean commit history

---
+++ README.md
@@ -0,0 +1,3 @@
+# JIMS
+
+When the master branch is changed, the site is automatically updated [here](https://prgrmmr.me/jims/).
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['email'])) header('Location: login.php');
+
+?>
+<html>
+<head>
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+	<link href="css/global.css" rel="stylesheet">
+	<link href="css/header.css" rel="stylesheet">
+	<link href="css/modules.css" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<section id="menu">
+			<ul>
+				<li><a href=".">Dashboard</a></li>
+				<li><a href="booking">Booking</a></li>
+			</ul>
+		</section>
+		<section id="user-links">
+			<ul>
+				<li><a href="user">role</a></li>
+				<li><a href="user"><i class="fa fa-fw fa-user"></i><span><?php echo $_SESSION['username']; ?></span></a></li>
+				<li><a href="logout.php"><i class="fa fa-fw fa-sign-out"></i></a></li>
+			</ul>
+		</section>
+	</header>
+	<div id="content">
+		<?php isset($_GET['module']) && file_exists('modules/' . $_GET['module'] . '.php') ? include('modules/' . $_GET['module'] . '.php') : include('modules/dashboard.php'); ?>
+	</div>
+</body>
+</html>
