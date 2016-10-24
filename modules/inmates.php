7cb59a5bb71b224af39007ebcfe033766b7b5f07
Aaron Walker <aw@prgrmmr.me>
2016-10-24 13:52:54 -0700

    Update list of inmates

--- modules/inmates.php
+++ modules/inmates.php
@@ -1,46 +1,25 @@
+<?php $db = new SQLite3("assets/data/jims.db"); ?>
+<?php $inmates = $db->query("SELECT * FROM inmates"); ?>
 <div id="booking">
 	<table>
-		<tr>
-			<th>#</th>
-			<th>Last, First</th>
-			<th>Date Booked</th>
-			<th>Offense</th>
-		</tr>
-		<tr>
-			<td>95092800</td>
-			<td>Urbanski, Aaron</td>
-			<td>09/28/1995</td>
-			<td>Forgery of a vehicle identification number</td>
-		</tr>
-		<tr>
-			<td>05011000</td>
-			<td>Mcintyre, Briana</td>
-			<td>01/10/2005</td>
-			<td>Inciting to riot</td>
-		</tr>
-		<tr>
-			<td>15122500</td>
-			<td>Hervey, James</td>
-			<td>12/25/2015</td>
-			<td>Criminal possession of an anti-security item</td>
-		</tr>
-		<tr>
-			<td>15122501</td>
-			<td>Khalil, Kyle</td>
-			<td>12/25/2015</td>
-			<td>Criminal sale of a controlled substance in or near school grounds</td>
-		</tr>
-		<tr>
-			<td>99032100</td>
-			<td>Hollister, Derek</td>
-			<td>03/21/1999</td>
-			<td>Endangering the welfare of a child</td>
-		</tr>
-		<tr>
-			<td>00113000</td>
-			<td>Schneps, Kevin</td>
-			<td>11/30/2000</td>
-			<td>Interference harassment or intimidation of a service animal</td>
-		</tr>
+			<tr>
+				<th colspan="2">List of Inmates</th>
+				<th>Offenses</th>
+			</tr>
+		<?php while ($inmate = $inmates->fetchArray()) : ?>
+			<tr>
+				<td class="id"><a href="#"><?php echo $inmate['id']; ?></a></td>
+				<td class="name"><a href="#"><?php echo $inmate['name']; ?></a></td>
+				<td class="offense">
+					<?php
+					$offenses = $db->query("SELECT charge FROM booking WHERE inmate='" . $inmate['id'] ."'");
+					$string = '';
+					while ($offense = $offenses->fetchArray())
+						$string .= $offense['charge'] . ', ';
+					echo substr($string, 0, -2);
+					?>
+				</td>
+			</tr>
+		<?php endwhile; ?>
 	</table>
 </div>
