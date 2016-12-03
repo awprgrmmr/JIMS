2881b3db4a4cc873bd82283d430bf7a7ecb806c4
awprgrmmr <aw@prgrmmr.me>
2016-12-02 18:37:03 -0800

    Added sign in button

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
 	<input value="email@email.com" type="email" name="email" autofocus/>
 	<label for="password">Password</label>
 	<input value="password" type="password" name="password" />
+	<br />
+	<input type="submit" value="Sign in" />
 	<input type="hidden" name="action" value="login" />
 </form>
 </body>
