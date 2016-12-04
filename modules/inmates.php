dfda0907949fbe2edb5226d3473fe1e175740ab4
Aaron Walker <aw@prgrmmr.me>
2016-12-04 13:36:38 -0800

    Added details to inmate page

--- modules/inmates.php
+++ modules/inmates.php
@@ -29,18 +29,34 @@
 		<h3><?php echo "Inmate #$inmate[id]"; ?></h3>
 		<div>
 			<form class="clearfix">
-				<label>Full Name (Last, First, Middle)</label>
-				<input type="text" name="last_name" placeholder="Last Name" value="<?php echo "$inmate[last_name]"; ?>"/>
-				<input type="text" name="first_name" placeholder="First Name" value="<?php echo "$inmate[first_name]"; ?>"/>
-				<input type="text" name="middle_name" placeholder="Middle Name" value="<?php echo "$inmate[middle_name]"; ?>"/>
-				<?php $offenses = $db->query("SELECT * FROM booking WHERE inmate='$inmate[id]'"); ?>
+				<label>Basic Details</label>
+				<span>First name</span><input type="text" name="first_name" value="<?php echo "$inmate[first_name]"; ?>"/>
+				<span>Middle name</span><input type="text" name="middle_name" value="<?php echo "$inmate[middle_name]"; ?>"/>
+				<span>Last name</span><input type="text" name="last_name" value="<?php echo "$inmate[last_name]"; ?>"/>
+				<label>Physical Description</label>
+				<span>Sex</span><select name="sex">
+					<option value="male"<?php if ($inmate['sex'] == 'male') echo ' selected'; ?>>Male</option>
+					<option value="female"<?php if ($inmate['sex'] == 'female') echo ' selected'; ?>>Female</option>
+					<option value="intersex"<?php if ($inmate['sex'] == 'intersex') echo ' selected'; ?>>Intersex</option>
+				</select>
+				<span>Height</span><input type="text" name="height" value="<?php echo $inmate['height']; ?>"/>
+				<span>Weight</span><input type="text" name="weight" value="<?php echo $inmate['weight']; ?>" />
 				<label>Charges</label>
 				<ul class="offenses">
+					<?php $offenses = $db->query("SELECT * FROM booking WHERE inmate='$inmate[id]'"); ?>
 					<?php while ($offense = $offenses->fetchArray()) : ?>
 						<li>
 							<span class="offense"><?php echo $offense['charge']; ?></span>
 							<span class="date"><?php echo date('F j, Y', $offense['datetime']); ?></span>
 						</li>
+					<?php endwhile; ?>
+				</ul>
+				<br />
+				<label>Property</label>
+				<ul class="property">
+					<?php $property = $db->query("SELECT description FROM property WHERE inmate='$inmate[id]'"); ?>
+					<?php while ($item = $property->fetchArray()) : ?>
+						<li><span><?php echo $item['description']; ?></span></li>
 					<?php endwhile; ?>
 				</ul>
 			</form>
