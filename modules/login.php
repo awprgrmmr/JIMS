4ddddde842d07aca23147a8773801fcb487ac3d5
Aaron Walker <aw@prgrmmr.me>
2016-12-04 10:28:38 -0800

    Removed login autofill

--- modules/login.php
+++ modules/login.php
@@ -1,8 +1,8 @@
 <form method="post">
 	<label for="email">Email</label>
-	<input value="email@email.com" type="email" name="email" autofocus/>
+	<input type="email" name="email" autofocus/>
 	<label for="password">Password</label>
-	<input value="password" type="password" name="password" />
+	<input type="password" name="password" />
 	<br />
 	<input type="submit" value="Sign in" />
 	<input type="hidden" name="action" value="login" />
